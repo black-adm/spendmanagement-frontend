ea1879e646b54257d13b30f22dc9400c49d3d5f6
black-adm <mattheuscontato17@gmail.com>
2023-10-01 00:32:38 -0300

    add axios react-hook-form hookform/resolvers and update form

--- src/app/login/components/Form.tsx
+++ src/app/login/components/Form.tsx
@@ -1,9 +1,23 @@
 'use client'
 
+import { useState } from "react";
+import { useForm } from "react-hook-form"
 import { Inputs } from "./Inputs";
 import { GoogleButton } from "./GoogleButton";
 
 export function Form() {
+    const [output, setOutput] = useState('')
+
+    const { handleSubmit, register ,watch } = useForm()
+
+    function loginData() {
+        const formData = {
+            email: watch('email'),
+            password: watch('password'),
+        };
+        setOutput(JSON.stringify(formData, null, 2));
+    };
+
     return (
         <>
             <div className="flex flex-1 flex-col justify-center space-y-5 max-w-md">
@@ -11,13 +25,23 @@ export function Form() {
                     <h2 className="text-2xl md:text-4xl font-bold">Fazer login na sua conta</h2>
                     <p className="text-base md:text-xl">Entre na sua conta para acessar o painel</p>
                 </div>
-                <div className="flex flex-col max-w-md space-y-5">
-                    <Inputs />
+                <form
+                    onSubmit={handleSubmit(loginData)}
+                    className="flex flex-col max-w-md space-y-5"
+                >
+                    <Inputs
+                        register={register}
+                    />
 
-                    <button className="flex items-center gap-x-2 justify-center flex-none px-3 py-2 md:px-4 md:py-3 border-2 rounded-lg font-medium border-black bg-black text-white">
-                        Acessar
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-in"><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"/><polyline points="10 17 15 12 10 7"/><line x1="15" x2="3" y1="12" y2="12"/></svg>
-                    </button>
+                    <div className="pt-0 sm:pt-8">
+                        <button
+                            type="submit"
+                            className="flex items-center gap-x-2 justify-center w-full px-3 py-2 md:px-4 md:py-3 border-2 rounded-lg font-medium border-black bg-black text-white hover:bg-primary-black focus:bg-primary-black transition-colors"
+                        >
+                            Acessar
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-log-in"><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" /><polyline points="10 17 15 12 10 7" /><line x1="15" x2="3" y1="12" y2="12" /></svg>
+                        </button>
+                    </div>
 
                     <div className="flex justify-center items-center">
                         <span className="w-full border border-black"></span>
@@ -26,7 +50,9 @@ export function Form() {
                     </div>
 
                     <GoogleButton />
-                </div>
+                </form>
+
+                <pre>{output}</pre>
             </div>
         </>
     )
