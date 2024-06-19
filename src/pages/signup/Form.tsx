f063c5d30fd894d6a5e32f33e5a3e8722c4d7fa3
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2024-06-19 18:58:24 -0300

    feat: finalizando criacao de usuarios e inserção de usuario em grupo

--- src/pages/signup/Form.tsx
+++ src/pages/signup/Form.tsx
@@ -17,36 +17,107 @@ export function SignUpForm() {
   async function handleSubmit(event: any) {
     event.preventDefault();
 
-    const formData = {
-      client_id: "receipts-api",
-      grant_type: "client_credentials",
-      client_secret: "7aMsLHaSjLR9KtURpqGgdfcYqdEa8zbb"
-    };
-
-    const formDataCreateUser = {
-      username: watch("username"),
-      email: watch("email"),
-      attributes: {
-        "zoneinfo": [watch("zoneinfo")],
-        "birthdate": [watch("birthdate")],
-        "phoneNumber": [watch("phoneNumber")],
-        "gender": [watch("gender")],
-        "fullname": [watch("fullname")],
-        "tenant": [100000],
-        "picture": [watch("picture")]
-      }
-    }
-
     try {
+
+      const formData = {
+        client_id: "receipts-api",
+        grant_type: "client_credentials",
+        client_secret: "7aMsLHaSjLR9KtURpqGgdfcYqdEa8zbb"
+      };
+
+      //Obter token pra usar em todos os requests
       const responseGetToken = await server.post("realms/10000/protocol/openid-connect/token", formData);
       const token = "Bearer " + await responseGetToken.data.access_token;
 
+      const nameParts = watch("fullname").split(" ");
+      const firstName = nameParts[0] || '';
+      const lastName = nameParts.slice(1).join(" ") || '';
 
-      console.log(JSON.stringify(formDataCreateUser));
+      const formDataCreateUser = {
+        enabled: true,
+        username: watch("username"),
+        firstName: firstName,
+        lastName: lastName,
+        email: watch("email"),
+        emailVerified: true,
+        attributes: {
+          "zoneinfo": [watch("zoneinfo")],
+          "birthdate": [watch("birthdate")],
+          "phoneNumber": [watch("phoneNumber")],
+          "gender": [watch("gender")],
+          "fullname": [watch("fullname")],
+          "tenant": [100000],
+          "picture": [watch("picture")]
+        }
+      }
 
+      //Criar usuario
       const responseSaveUser = await server.post("admin/realms/10000/users", formDataCreateUser, {
-        headers: { "Authorization": token }
+        headers: {
+          "Authorization": token,
+          "Content-Type": "application/json"
+        },
       });
+
+      // Se criado com sucesso, devemos buscar pelo nome de usuário para obter o ID
+      if (responseSaveUser.status === 201) {
+        const responseGetUser = await server.get(`admin/realms/10000/users`, {
+          headers: {
+            "Authorization": token,
+            "Content-Type": "application/json"
+          },
+          params: { email: watch("email") }
+        });
+
+        const userId = responseGetUser.data[0].id;
+        console.log(userId);
+
+        //Em seguida deve-se definir uma senha para o usuário, seguindo o que foi escrito no campo.
+        const passwordData = {
+          type: "password",
+          temporary: false,
+          value: watch("password")
+        };
+
+        await server.put(`admin/realms/10000/users/${userId}/reset-password`, passwordData, {
+          headers: {
+            "Authorization": token,
+            "Content-Type": "application/json"
+          }
+        });
+
+        // Obter todos os grupos do realm
+        const groupsResponse = await server.get(`admin/realms/10000/groups`, {
+          headers: {
+            Authorization: token,
+            "Content-Type": "application/json"
+          }
+        });
+
+        const groups = groupsResponse.data;
+
+        // Mapear os IDs dos grupos que você deseja adicionar o usuário
+        const groupsToAdd = ["Api Readers", "Api Writers"]; // Nomes dos grupos que você quer adicionar o usuário
+        const addGroupsPromises = [];
+
+        groupsToAdd.forEach(groupName => {
+          const group = groups.find((g: { id: string, name: string }) => g.name === groupName);
+          if (group) {
+            const groupId = group.id;
+            addGroupsPromises.push(
+              server.put(`admin/realms/10000/users/${userId}/groups/${groupId}`, {}, {
+                headers: {
+                  Authorization: token,
+                  "Content-Type": "application/json"
+                }
+              })
+            );
+          } else {
+            console.error(`Group '${groupName}' not found`);
+          }
+        });
+      }
+
 
       toast.success(
         "Login realizado com sucesso!", {
